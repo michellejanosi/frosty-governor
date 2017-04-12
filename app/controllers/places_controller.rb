b40839bc0507cf7ddcfeb75875d5047e87d3061e
michellejanosi <michelle.janosi@gmail.com>
2017-04-12 08:44:51 -0400

    Added validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -28,16 +32,22 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render text: 'Sorry, governor! You do not have access to update this pub. 😔', status: :forbidden
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render text: 'Sorry, governor! You do not have acess to delete this pub. 😔', status: :forbidden
     end
