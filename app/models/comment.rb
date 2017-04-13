6b1e48230c4e23d92ce0b298ae8910eab09e20fd
michellejanosi <michelle.janosi@gmail.com>
2017-04-13 09:47:34 -0400

    Added function to ratings ⭐️

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    '1 star': '⭐️',
+    '2 stars': '⭐️⭐️',
+    '3 stars': '⭐️⭐️⭐️',
+    '4 stars': '⭐️⭐️⭐️⭐️',
+    '5 stars': '⭐️⭐️⭐️⭐️⭐️'
+  }
 end
