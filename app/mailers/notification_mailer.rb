87b68c5f355ff7592171dd9daea6d43742f77fbe
michellejanosi <michelle.janosi@gmail.com>
2017-04-17 11:16:05 -0400

    Adjusted the automated email mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@frostygovernor.co.uk"
 
-  def comment_added
-    mail(to: "michellexj11@gmail.com",
-      subject: "A comment has been added to your pub 🍻")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name} 🍻")
   end
 end
